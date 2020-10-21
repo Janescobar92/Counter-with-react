10efe8b760023b3afd77de4dd39efd5f6601357c
Janescobar92 <jancore92@gmail.com>
2020-10-21 16:27:25 +0000

    counter props added

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from "react";
 
 import PropTypes from "prop-types";
 
-export const MyCounter = () => {
+export const MyCounter = props => {
 	const [timer, setTimer] = useState(0);
 	useEffect(() => {
 		const interval = setInterval(() => {
 			setTimer(seconds => seconds + 1);
-		}, 1000);
-		return () => clearInterval({ interval });
+		}, props.seconder);
+		return () => clearInterval(interval);
 	}, []);
 
 	return (
 		<div className="numberBox text-center">
-			<span>{timer}</span>
+			<span>{timer % 10}</span>
 		</div>
 	);
+};
+
+MyCounter.propTypes = {
+	seconder: PropTypes.number
 };
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,14 +4,16 @@ import { Mycounter } from "./counter.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+
 import { MyCounter } from "./counter.jsx";
 
 //create your first component
 export function Home() {
 	const numberOfBoxes = 4;
 	const items = [];
+	const tickers = [100000, 10000, 1000, 100];
 	for (let i = 0; i < numberOfBoxes; i++) {
-		items.push(<MyCounter />);
+		items.push(<MyCounter seconder={tickers[i]} />);
 	}
 
 	return <div className="mt-5 d-flex justify-content-center">{items}</div>;
